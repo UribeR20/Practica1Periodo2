6f872ed83967fa2d33d6261f2743bd51ab04eb27
Rodrigo Aguilar <rodrigo.aguilar@catolica.edu.sv>
2023-04-15 15:34:15 -0600

    Practica2 Periodo2

---
+++ Practica1Periodo2/Controllers/equiposController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practica1Periodo2.Controllers
+{
+    public class equiposController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
